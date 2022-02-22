8a4fa1b8eec4dac8f094dcf00d74eb0867428164
mystickoder <89115482+mystickoder@users.noreply.github.com>
2022-02-22 09:00:29 -0500

    Add files via upload

---
+++ willies_fishcamp.py
@@ -0,0 +1,31 @@
+print("Welome to Willies Fish Camp. Please place your order.")
+sandwich = ['grouper', 'tuna', 'salmon']
+sides= ['fries', 'cole slaw', 'hush puppies']
+drinks = ['water', 'sprite', 'coke']
+
+if "grouper" in sandwich:
+    print("add fries")
+    
+
+if "fries" in sides:
+    print("add sprite")
+    
+if "tuna" in sandwich:
+    print("add cole slaw")
+
+else:
+    print("we have not slaw")
+    
+if 'salmon' in sandwich:
+    print("add hush puppies")
+    
+if "hush puppies" in sides:
+    print("add a coke to my order")
+    
+    
+if "coke" not in "drinks":
+    print("do you have Pepsi Please? ")
+    
+    
+print("Your order is up")
+print("Thanks for visiting Willies Fish Camp!")
